4eabcee0fb76a895789698252b8b0d4875fe357e
cepoumian <cepoumian@gmail.com>
2026-01-17 14:52:10 -0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require("http");
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello desde CI/CD con Jenkins\n");
+});
+
+server.listen(port, () => {
+  console.log(`Server escuchando en puerto ${port}`);
+});
